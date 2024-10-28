73acebcb599e2e4abef6aac2a4674e2b6d46d5d3
Jinseo Jeong <104565869+giikbukjin@users.noreply.github.com>
2024-10-28 21:26:40 +0900

    [Bronze V] Title: A/B, Time: 180 ms, Memory: 17768 KB -BaekjoonHub

---
+++ 백준/Bronze/1008. A／B/A／B.java
@@ -0,0 +1,10 @@
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        double A = scanner.nextInt();
+        double B = scanner.nextInt();
+        System.out.println(A / B);
+    }
+}
