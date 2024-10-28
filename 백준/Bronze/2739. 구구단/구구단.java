06fe17e4b992ff143922610cfc4a29f404af5039
Jinseo Jeong <104565869+giikbukjin@users.noreply.github.com>
2024-10-28 22:14:26 +0900

    [Bronze V] Title: 구구단, Time: 204 ms, Memory: 18528 KB -BaekjoonHub

---
+++ 백준/Bronze/2739. 구구단/구구단.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        for (int i = 1; i <= 9; i++) {
+            System.out.println(N + " * " + i + " = " + N * i);
+        }
+    }
+}
