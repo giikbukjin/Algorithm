cca76f5f7ecdb6f68be85e137b1459a8a8be8589
Jinseo Jeong <104565869+giikbukjin@users.noreply.github.com>
2024-10-28 22:15:38 +0900

    [Bronze V] Title: N 찍기, Time: 852 ms, Memory: 32388 KB -BaekjoonHub

---
+++ 백준/Bronze/2741. N 찍기/N 찍기.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        Scanner scanner = new Scanner(System.in);
+        int N = scanner.nextInt();
+        for (int i = 0; i < N; i++) {
+            System.out.println(i + 1);
+        }
+    }
+}
