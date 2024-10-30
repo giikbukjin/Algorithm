24b6091a7c32682e7e8bf69ed9d7c9020623d977
Jinseo Jeong <104565869+giikbukjin@users.noreply.github.com>
2024-10-30 11:14:41 +0900

    [Bronze III] Title: 최소, 최대, Time: 472 ms, Memory: 92160 KB -BaekjoonHub

--- 백준/Bronze/10818. 최소， 최대/최소， 최대.java
+++ 백준/Bronze/10818. 최소， 최대/최소， 최대.java
@@ -11,7 +11,18 @@ public class Main {
         for (int i = 0; i < N; i++) { // 정수 배열에 저장
             arr[i] = Integer.parseInt(sb.nextToken());
         }
-        System.out.print(Arrays.stream(arr).min().getAsInt() + " ");
-        System.out.print(Arrays.stream(arr).max().getAsInt());
+
+        int min = arr[0];
+        int max = arr[0];
+        for (int i = 0; i < N; i++) {
+            if (arr[i] > max) {
+                max = arr[i];
+            } else if (arr[i] < min) {
+                min = arr[i];
+            }
+        }
+        System.out.println(min + " " + max);
+//        System.out.print(Arrays.stream(arr).min().getAsInt() + " ");
+//        System.out.print(Arrays.stream(arr).max().getAsInt());
     }
 }
