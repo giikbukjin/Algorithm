a3ba7dc31f31df4a4cdf20224648bd3b5937a2cb
Jinseo Jeong <104565869+giikbukjin@users.noreply.github.com>
2024-11-01 10:39:15 +0900

    [Bronze V] Title: 개, Time: 100 ms, Memory: 13868 KB -BaekjoonHub

---
+++ 백준/Bronze/10172. 개/개.java
@@ -0,0 +1,12 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        System.out.println("|\\_/|");
+        System.out.println("|q p|   /}");
+        System.out.println("( 0 )\"\"\"\\");
+        System.out.println("|\"^\"`    |");
+        System.out.println("||_/=\\\\__|");
+    }
+}
