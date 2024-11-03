89819b503202654b456657b8d4b05060e08d7161
Jinseo Jeong <104565869+giikbukjin@users.noreply.github.com>
2024-11-03 16:22:08 +0900

    [Bronze I] Title: 평균, Time: 108 ms, Memory: 14392 KB -BaekjoonHub

--- 백준/Bronze/1546. 평균/평균.java
+++ 백준/Bronze/1546. 평균/평균.java
@@ -1,31 +1,25 @@
-import java.util.*;
 import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         int N = Integer.parseInt(br.readLine());
-
         StringTokenizer st = new StringTokenizer(br.readLine(), " ");
-
         double[] arr = new double[N];
-        double max = 0;
-        double sum = 0;
+        double M = 0.0;
+        double sum = 0.0;
 
         for (int i = 0; i < N; i++) {
-            arr[i] = Double.parseDouble(st.nextToken());
-
-            if (max < arr[i]) {
-                max = arr[i];
+            arr[i] = Integer.parseInt(st.nextToken());
+            if (arr[i] > M) {
+                M = arr[i];
             }
         }
-
         for (int i = 0; i < N; i++) {
-            arr[i] = (arr[i] / max) * 100.0;
-
+            arr[i] = arr[i] / M * 100.0;
             sum += arr[i];
         }
-
         System.out.println(sum / N);
     }
 }
