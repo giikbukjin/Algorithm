76af5ac4b94fe22440262ba98aac1dbce5372552
Jinseo Jeong <104565869+giikbukjin@users.noreply.github.com>
2024-11-03 23:43:03 +0900

    [Silver II] Title: 스택 수열, Time: 296 ms, Memory: 27536 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,50 +1,48 @@
-import java.io.BufferedReader;
-import java.io.IOException;
-import java.io.InputStreamReader;
-import java.util.Stack;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Main {
     public static void main(String[] args) throws IOException {
+        System.out.print(fx());
+    }
+
+    static public String fx() throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringBuilder sb = new StringBuilder();
-
-        int stackNum = Integer.parseInt(br.readLine()); // 수열의 개수
-        int[] array = new int[stackNum];
         Stack<Integer> stack = new Stack<>();
-        int num = 1; // 1씩 증가하는 자연수
-        boolean result = true;
+        int n = Integer.parseInt(br.readLine());
+        int num = 1;
 
-        for (int i = 0; i < stackNum; i++) { // 수열 값 저장
-            StringTokenizer st = new StringTokenizer(br.readLine());
-
-            while (st.hasMoreTokens()) {
-                array[i] = Integer.parseInt(st.nextToken());
-            }
+        int first = Integer.parseInt(br.readLine());
+        int prev = first;
+        for (int i = 0; i < first; i++) {
+            sb.append("+").append("\n");
+            stack.push(num);
+            num++;
         }
+        stack.pop();
+        sb.append("-").append("\n");
 
-        for (int i = 0; i < stackNum; i++) {
-            if (num <= array[i]) {
-                while (num <= array[i]) {
+        for (int i = 1; i < n; i++) {
+            int current = Integer.parseInt(br.readLine());
+            if (current > prev) {
+                while (num <= current) {
+                    sb.append("+").append("\n");
                     stack.push(num);
                     num++;
-                    sb.append("+\n");
                 }
                 stack.pop();
-                sb.append("-\n");
+                sb.append("-").append("\n");
             } else {
                 int top = stack.pop();
-                if (top == array[i]) {
-                    sb.append("-\n");
-                } else {
-                    System.out.println("NO");
-                    result = false;
-                    break;
+                sb.append("-").append("\n");
+                if (top != current) {
+                    return "NO";
                 }
             }
+            prev = current;
         }
-        if (result) {
-            System.out.println(sb);
-        }
+        System.out.print(sb);
+        return "";
     }
 }
