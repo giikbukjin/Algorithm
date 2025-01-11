c56a97dbe8b19e6d5e888de7ddef4d5adee78610
Jinseo Jeong <104565869+giikbukjin@users.noreply.github.com>
2025-01-11 12:01:09 +0900

    [Silver IV] Title: 설탕 배달, Time: 104 ms, Memory: 14208 KB -BaekjoonHub

--- 백준/Silver/2839. 설탕 배달/설탕 배달.java
+++ 백준/Silver/2839. 설탕 배달/설탕 배달.java
@@ -7,15 +7,15 @@ public class Main {
         int N = Integer.parseInt(br.readLine());
         int bags = 0;
 
-        for (int i = N / 5; i >= 0 ; i--) {
-            if ((N - 5 * i) % 3 == 0) {
+        for (int i = N / 5; i >= 0 ; i--) { // 5kg 최대 배달 개수에서 시작해 하나씩 줄여 가면서
+            if ((N - 5 * i) % 3 == 0) { // 3kg까지 배달했을 때 나머지가 0인 경우를 찾는다.
                 bags += i;
                 bags += (N - 5 * i) / 3;
                 break;
             }
         }
         if (bags == 0) {
-            System.out.println(-1);
+            System.out.println(-1); // 정확하게 Nkg가 아니면 -1을 반환한다.
         } else {
             System.out.println(bags);
         }
